f9adebc129e5a096b0ffa0564fb8eca190609fa5
zuiguangyin <2247359268@qq.com>
2019-10-10 22:39:40 +0800

    Spring HelloWorld！

--- spring/src/main/java/com/zgy/learn/springhelloworld/MainHelloSpring.java
+++ spring/src/main/java/com/zgy/learn/springhelloworld/MainHelloSpring.java
@@ -5,15 +5,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @Author: renjiaxin
- * @Despcription:
+ * @Despcription: Spring HelloWorld！
  * @Date: Created in 2019/10/10 22:31
  * @Modified by:
  */
 public class MainHelloSpring {
 
     public static void main(String[] args) {
+        // 1.创建Spring IOC容器
         ApplicationContext act = new ClassPathXmlApplicationContext("spring-helloworld.xml");
+        // 2.从容器中获取Bean
         HelloSpring hs = (HelloSpring) act.getBean("zhangsan");
+        // 3.调用方法
         System.out.println(hs.toString());
     }
 }
