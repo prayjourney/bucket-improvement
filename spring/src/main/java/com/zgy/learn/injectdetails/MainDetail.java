d24640d5c90d45ca6e329720e92458cb326cc97f
zuiguangyin <2247359268@qq.com>
2019-10-11 21:33:17 +0800

    null值的使用， 添加操作注释

--- spring/src/main/java/com/zgy/learn/injectdetails/MainDetail.java
+++ spring/src/main/java/com/zgy/learn/injectdetails/MainDetail.java
@@ -11,28 +11,44 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class MainDetail {
     public static void main(String[] args) {
+        // 1.新建Spring容器对象
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-inject-details.xml");
+        // 2.通过容器获得对象
         Poet p1 = (Poet) ctx.getBean("fangwenshan");
-        Poet p2 = (Poet) ctx.getBean("xusong");
+        // 3.独享的操作
         Poet p3 = (Poet) ctx.getBean("lanxiaoxie");
+
+        // 处理特殊字符
+        Poet p2 = (Poet) ctx.getBean("xusong");
         System.out.println(p1);
         System.out.println(p2);
         System.out.println(p3);
+
+        // 引用bean
         Song s1 = (Song) ctx.getBean("qinghuaci");
         Song s2 = (Song) ctx.getBean("faruxue");
         Song s3 = (Song) ctx.getBean("qingmingyushang");
+        // 内部bean
         Song s4 = (Song) ctx.getBean("juejiang");
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
         System.out.println(s4);
+
+        // 测试list注入
         Singer jay = (Singer) ctx.getBean("jay");
         jay.getAllSongs();
 
+        // 测试map注入
         SuperSingers singers2008 = (SuperSingers) ctx.getBean("superSingers2008");
         System.out.println(singers2008.toString());
 
+        // 测试map注入
         SuperSingers2 singers2010 = (SuperSingers2) ctx.getBean("superSingers2010");
         System.out.println(singers2010.toString());
+
+        // 测试null
+        Song songOld = (Song)ctx.getBean("null4Song");
+        System.out.println(songOld);
     }
 }
