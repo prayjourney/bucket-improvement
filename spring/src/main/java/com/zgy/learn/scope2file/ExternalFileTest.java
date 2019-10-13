0122f7857a9e5fe8c1ac30f00ae3776d82f03872
zuiguangyin <2247359268@qq.com>
2019-10-14 00:47:12 +0800

    bean的作用域

--- spring/src/main/java/com/zgy/learn/scope2file/ExternalFileTest.java
+++ spring/src/main/java/com/zgy/learn/scope2file/ExternalFileTest.java
@@ -12,32 +12,42 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ExternalFileTest {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-scope-and-externalfile.xml");
-        Boy zgy = (Boy)context.getBean("zgy");
+        Boy zgy = (Boy) context.getBean("zgy");
         System.out.println(zgy.toString());
 
         // 测试继承 parent
-        Human h1 = (Human)context.getBean("beijingren1");
+        Human h1 = (Human) context.getBean("beijingren1");
         System.out.println(h1.toString());
-        Human h2 = (Human)context.getBean("beijingren2");
+        Human h2 = (Human) context.getBean("beijingren2");
         System.out.println(h2.toString());
-        Human h3 = (Human)context.getBean("beijingren3");
+        Human h3 = (Human) context.getBean("beijingren3");
         System.out.println(h3.toString());
 
         // 测试依赖 depends-on
-        Boy boy2 = (Boy)context.getBean("boy2");
+        Boy boy2 = (Boy) context.getBean("boy2");
         System.out.println(boy2.toString());
 
-        Boy boy3 = (Boy)context.getBean("boy3");
+        Boy boy3 = (Boy) context.getBean("boy3");
         System.out.println(boy3.toString());
 
-        Boy sb = (Boy)context.getBean("superBoy");
+        Boy sb = (Boy) context.getBean("superBoy");
         System.out.println(sb);
 
         // 测试p空间
-        Boy sbBoy = (Boy)context.getBean("sbBoy2");
+        Boy sbBoy = (Boy) context.getBean("sbBoy2");
         System.out.println(sbBoy);
-        SuperCar scar = (SuperCar)context.getBean("car2");
+        SuperCar scar = (SuperCar) context.getBean("car2");
         System.out.println(scar);
+
+        // 测试bean的scope human1是prototype，每次返回不同对象, human2是single，每次返回相同对象
+        Human human11 = (Human) context.getBean("human1");
+        Human human12 = (Human) context.getBean("human1");
+        System.out.println("human11==human12: " + (human11 == human12));
+
+        Human human21 = (Human) context.getBean("human2");
+        Human human22 = (Human) context.getBean("human2");
+        System.out.println("human21==human22: " + (human21 == human22));
+
 
     }
 }
