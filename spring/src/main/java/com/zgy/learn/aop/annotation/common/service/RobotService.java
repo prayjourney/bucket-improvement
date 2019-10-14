7946c14dcb2f6ddfd576d1468394ed0299e9cb23
zuiguangyin <2247359268@qq.com>
2019-10-14 23:19:52 +0800

    aspectj

---
+++ spring/src/main/java/com/zgy/learn/aop/annotation/common/service/RobotService.java
@@ -0,0 +1,28 @@
+package com.zgy.learn.aop.annotation.common.service;
+
+import com.zgy.learn.aop.annotation.common.pojo.Robot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RobotService {
+    @Autowired
+    Robot robot;
+
+    public void openRobot(){
+        robot.openRobot();
+    }
+
+    public void sweepFloor() {
+        robot.sweepFloor();
+    }
+
+    public void infuseTea() {
+        robot.infuseTea();
+    }
+
+    public void closeRobot(){
+        robot.closeRobot();
+    }
+
+}
