a2c0975db17629b0a8c98040602a53cd5b89fc6c
zuiguangyin <2247359268@qq.com>
2019-10-15 03:13:37 +0800

    构造器引用的使用，有返回一个对象，然后就是参数的多少，决定了我们使用哪个函数式接口，匹配到的构造器的形参和函数式接口形参一致

---
+++ java8/src/main/java/com/zgy/learn/java8/lambda/yinyong/constructor/Laptop.java
@@ -0,0 +1,82 @@
+package com.zgy.learn.java8.lambda.yinyong.constructor;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription:
+ * @Date: Created in 2019/10/15 3:03
+ * @Modified by:
+ */
+public class Laptop {
+    private String name;
+
+    private String type;
+
+    private double price;
+    private String time;
+
+    public Laptop(String name) {
+        this.name = name;
+    }
+
+    public Laptop(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+    // 无法创建(String name, String time)
+
+
+    public Laptop(String name, String type, double price) {
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+
+    public Laptop(String name, String type, double price, String time) {
+        this.name = name;
+        this.type = type;
+        this.price = price;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", price=" + price +
+                ", time='" + time + '\'' +
+                '}';
+    }
+}
---
+++ java8/src/main/java/com/zgy/learn/java8/lambda/yinyong/constructor/LaptopCreate.java
@@ -0,0 +1,21 @@
+package com.zgy.learn.java8.lambda.yinyong.constructor;
+
+import java.util.function.Function;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription:
+ * @Date: Created in 2019/10/15 3:06
+ * @Modified by:
+ */
+public class LaptopCreate {
+    public static void main(String[] args) {
+        // 一定要是参数要对应一致
+        Function<String, Laptop> fun1 = (n1) -> new Laptop(n1);
+        Function<String, Laptop> fun2 = Laptop::new;
+        System.out.println(fun1.apply("索尼"));
+        Laptop laptop = fun2.apply("Mac");
+        System.out.println(laptop);
+
+    }
+}
