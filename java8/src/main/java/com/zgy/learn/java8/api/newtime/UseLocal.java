f23b26a2349b6f2f7d5f2c90bcad8dbfddee3709
zuiguangyin <2247359268@qq.com>
2019-10-18 03:47:30 +0800

    获取当前日期时间，构造日期时间

--- java8/src/main/java/com/zgy/learn/java8/api/newtime/UseLocal.java
+++ java8/src/main/java/com/zgy/learn/java8/api/newtime/UseLocal.java
@@ -1,6 +1,7 @@
 package com.zgy.learn.java8.api.newtime;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * @Author: renjiaxin
@@ -14,10 +15,18 @@ public class UseLocal {
         LocalDate ld = LocalDate.now();
         System.out.println(ld);
         // 创建日期
-        LocalDate ld2 = LocalDate.of(2018,3,5);
+        LocalDate ld2 = LocalDate.of(2018, 3, 5);
         System.out.println(ld2);
 
 
-
+        System.out.println("===================");
+        // 日期时间 ，获取当前
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println(ldt);
+        // 创建日期时间
+        LocalDateTime ldt2 = LocalDateTime.of(2018, 3, 5, 3, 4);
+        LocalDateTime ldt3 = LocalDateTime.of(2018, 3, 5, 3, 4, 22);
+        System.out.println(ldt2);
+        System.out.println(ldt3);
     }
 }
