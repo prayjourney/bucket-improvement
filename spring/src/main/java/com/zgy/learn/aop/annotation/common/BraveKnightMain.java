439637d59ffa7828d0e7184c750be661d999c5e9
zuiguangyin <2247359268@qq.com>
2019-10-22 23:41:31 +0800

    配置log4j日志

--- spring/src/main/java/com/zgy/learn/aop/annotation/common/BraveKnightMain.java
+++ spring/src/main/java/com/zgy/learn/aop/annotation/common/BraveKnightMain.java
@@ -1,14 +1,19 @@
 package com.zgy.learn.aop.annotation.common;
 
 import com.zgy.learn.aop.annotation.common.pojo.BraveKnight;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class BraveKnightMain {
+    private static Logger logger = LoggerFactory.getLogger(BraveKnight.class);
     public static void main(String[] args) {
         ApplicationContext context =new ClassPathXmlApplicationContext();
         ApplicationContext ac = new ClassPathXmlApplicationContext("spring-aop-annotation2.xml");
         BraveKnight br = (BraveKnight) ac.getBean("knight");
+        logger.debug("准备调用 %s 方法", "br.saying()");
         br.saying();
+        logger.info("%s 方法调用完毕", "br.saying()");
     }
 }
