fbac938a982bc8964232176ab8e8917d536420d7
renjiaxin <2247359268@qq.com>
2019-11-04 01:37:19 +0800

    controller给jsp页面传了一个值,通过model, modelmap来传值,还有后续的modelandview

--- mvc/springmvc1/src/main/java/com/zgy/learn/controller/UseRequestMappingController.java
+++ mvc/springmvc1/src/main/java/com/zgy/learn/controller/UseRequestMappingController.java
@@ -2,6 +2,7 @@ package com.zgy.learn.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,7 +27,12 @@ public class UseRequestMappingController {
     }
 
     @RequestMapping(value = "hello", method = RequestMethod.GET)
-    public String helloMap(String name) {
+    public String helloMap(ModelMap map, String name) {
+        /**
+         * 传值, 在方法中, 或者在url之中,其实都是在方法之中的意思, 一样的, 点击一下之后, 就会是一个url带值得样子了
+         * 我们可以使用Model, ModelMap, ModelAndView来, 将后端的值, 传给前端的页面等
+         */
+        map.addAttribute("name",name);
         System.out.println("hello " + name);
         return "hello";
     }
