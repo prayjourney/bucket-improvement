055e0ce39d2f441a883864bc1ccf4073699914a9
renjiaxin <2247359268@qq.com>
2019-11-03 19:45:41 +0800

    Spring mvc helloworld的pom.xml依赖, web.xml配置, 页面和controller

---
+++ mvc/springmvc1/src/main/java/com/zgy/learn/controller/HelloMVC.java
@@ -0,0 +1,24 @@
+package com.zgy.learn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: renjiaxin
+ * @Description:
+ * @Date: 2019-11-03 16:10
+ * @Modified by:
+ */
+@Controller
+public class HelloMVC{
+    /**
+     * 通过视图解析器InternalResourceViewResolver, 将视图解析为实际的物理视图, 会解析为
+     * prefix + returnvalue + suffix, 然后转发的相应的视图上去
+     * @return
+     */
+    @RequestMapping(value ="hellospringmvc")
+    public String hello() {
+        System.out.println("hello spring mvc!");
+        return "success";
+    }
+}
