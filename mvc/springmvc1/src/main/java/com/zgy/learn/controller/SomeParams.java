d5975d4f78645d1858ccfb648667ab58b4374720
renjiaxin <2247359268@qq.com>
2019-11-04 15:40:36 +0800

    cookie和header的例子需要补齐

--- mvc/springmvc1/src/main/java/com/zgy/learn/controller/SomeParams.java
+++ mvc/springmvc1/src/main/java/com/zgy/learn/controller/SomeParams.java
@@ -1,6 +1,7 @@
 package com.zgy.learn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,6 +19,11 @@ public class SomeParams {
                             @RequestParam(required = true, defaultValue = "0") int age,
                             @RequestParam(required = false) String gender) {
         // 其实这个都不需要任何返回的
+        return "testparam";
+    }
+    @RequestMapping(value = "head")
+    public String TestParamHead(@RequestHeader String name){
+        // 其实这个都不需要任何返回的, 还有一个是cookie
         return "testparam";
     }
 
