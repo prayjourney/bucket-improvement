65b702a75b5944b8382155c3933997396fe30e1c
renjiaxin <2247359268@qq.com>
2019-11-09 03:03:04 +0800

    post配合表单域,来实现post替代put和delete,后台没什么问题,但是前端显示405错误.

---
+++ mvc/springmvc1/src/main/java/com/zgy/learn/controller/Post2PutDeleteController.java
@@ -0,0 +1,37 @@
+package com.zgy.learn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+/**
+ * @Author: renjiaxin
+ * @Description: Post方法模拟Put和Delete, from表单只是支持get 和post这两个标准的
+ * @Date: 2019-11-09 01:34
+ * @Modified by:
+ */
+@Controller
+public class Post2PutDeleteController {
+    @RequestMapping(value = "post2put/{name}", method = RequestMethod.PUT)
+    public String post2Put(@PathVariable("name") String name, ModelMap map) {
+        String newName = "new Name";
+        System.out.println("我更新了name: " + name +", 新的值是: "  + newName);
+        map.addAttribute("oldname", name);
+        map.addAttribute("newname", newName);
+        return "post2put";
+
+    }
+
+    @RequestMapping(value = "post2delete/{id}", method = RequestMethod.DELETE)
+    public String post2Delete(@PathVariable("id") Integer id, Map<String, Integer> mp) {
+        System.out.println("我删除了id: " + id);
+        mp.put("deleteid", id);
+        return "post2delete";
+
+    }
+}
---
+++ mvc/springmvc1/src/main/webapp/WEB-INF/views/post2put.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>put请求方法使用post来实现</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<h2 class="text-danger">修改之前的名字${requestScope.oldname}</h2>
+<h2 class="text-info">修改之后的名字${newname}</h2>
+
+</body>
+</html>
