caab3ce1a136f5e2ecd292eea83750c96e509257
renjiaxin <2247359268@qq.com>
2019-12-01 11:09:43 +0800

    @ExceptionHandler的方法, 当有多个异常处理器的时候, 总是匹配和异常最相近的异常处理器

--- mvccrud/src/main/java/com/zgy/learn/crud/handlers/MyResolverExceptionController.java
+++ mvccrud/src/main/java/com/zgy/learn/crud/handlers/MyResolverExceptionController.java
@@ -31,14 +31,15 @@ public class MyResolverExceptionController {
      * 1. 对应的某一种类型的异常, 写到Handler注解之中, 然后出现了异常, 它就会去处理
      * 2. @ExceptionHandlerd的入参之中可以加入 Exception类型 的参数, 该参数对应的是发生异常的对象
      * 3. @ExceptionHandlerd的方法入参之中不能加入map, 如果要给页面返回异常, 那就要使用ModelAndView
+     * 4. @ExceptionHandler的方法, 当有多个异常处理器的时候, 总是匹配和异常最相近的异常处理器
      */
-    @ExceptionHandler({ArithmeticException.class, OutOfMemoryError.class})
-    public ModelAndView handleArithmeticException(Exception ex) {
-        System.out.println("出了异常");
-        ModelAndView mv = new ModelAndView("error");
-        mv.addObject("errorException", ex);
-        return mv;
-    }
+//    @ExceptionHandler({ArithmeticException.class, OutOfMemoryError.class})
+//    public ModelAndView handleArithmeticException(Exception ex) {
+//        System.out.println("出了异常");
+//        ModelAndView mv = new ModelAndView("error");
+//        mv.addObject("errorException", ex);
+//        return mv;
+//    }
 
     @ExceptionHandler({RuntimeException.class})
     public ModelAndView handleArithmeticException2(Exception ex) {
