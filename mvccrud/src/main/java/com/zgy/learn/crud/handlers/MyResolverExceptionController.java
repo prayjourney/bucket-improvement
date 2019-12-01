7c91a6b42e858ad1ac24c7ce97d11feade702001
renjiaxin <2247359268@qq.com>
2019-12-01 11:04:46 +0800

    添加错误页面, 把错误传给页面显示

--- mvccrud/src/main/java/com/zgy/learn/crud/handlers/MyResolverExceptionController.java
+++ mvccrud/src/main/java/com/zgy/learn/crud/handlers/MyResolverExceptionController.java
@@ -1,9 +1,11 @@
 package com.zgy.learn.crud.handlers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
@@ -18,17 +20,23 @@ public class MyResolverExceptionController {
 
     // 业务可能会发生异常
     @RequestMapping(value = "testexception")
-    public String testException(@RequestParam int i, Map<String, Integer> mpmp){
+    public String testException(@RequestParam int i, Map<String, Integer> mpmp) {
         System.out.println(i);
-        mpmp.put("res", 10/i);
+        mpmp.put("res", 10 / i);
         return "arithmeticresult";
 
     }
 
-    // 对应的某一种类型的异常, 写到Handler注解之中, 然后出现了异常, 它就会去处理
+    /**
+     * 1. 对应的某一种类型的异常, 写到Handler注解之中, 然后出现了异常, 它就会去处理
+     * 2. @ExceptionHandlerd的入参之中可以加入 Exception类型 的参数, 该参数对应的是发生异常的对象
+     * 3. @ExceptionHandlerd的方法入参之中不能加入map, 如果要给页面返回异常, 那就要使用ModelAndView
+     */
     @ExceptionHandler({ArithmeticException.class, OutOfMemoryError.class})
-    public String handleArithmeticException(){
+    public ModelAndView handleArithmeticException(Exception ex) {
         System.out.println("出了异常");
-        return "error";
+        ModelAndView mv = new ModelAndView("error");
+        mv.addObject("errorException", ex);
+        return mv;
     }
 }
---
+++ mvccrud/src/main/webapp/WEB-INF/views/error.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+    <head>
+        <title>Error page</title>
+    </head>
+    <body>
+        <p>
+            出错了！<br>
+            错误是: ${errorException}
+        </p>
+        
+    </body>
+</html>
