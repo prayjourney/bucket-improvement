e34b8cf06a8ae5383c40b8f5d169cef2850e6058
renjiaxin <2247359268@qq.com>
2019-12-01 15:31:30 +0800

    update

--- mvccrud/src/main/java/com/zgy/learn/crud/handlers/MyResolverExceptionController.java
+++ mvccrud/src/main/java/com/zgy/learn/crud/handlers/MyResolverExceptionController.java
@@ -1,11 +1,8 @@
 package com.zgy.learn.crud.handlers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
