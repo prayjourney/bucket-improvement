7269837db468119f35cb1d44b49f8ed122c889ff
renjiaxin <2247359268@qq.com>
2019-12-03 01:18:51 +0800

    暂时排除数据源的自动配置

--- bootstart/src/main/java/com/zgy/learn/bootstart/BootstartApplication.java
+++ bootstart/src/main/java/com/zgy/learn/bootstart/BootstartApplication.java
@@ -2,8 +2,9 @@ package com.zgy.learn.bootstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class BootstartApplication {
 
     public static void main(String[] args) {
