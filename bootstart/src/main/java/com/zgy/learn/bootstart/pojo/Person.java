78b0b76c97375fcd0c7251eb12a70a2e67a251f0
renjiaxin <2247359268@qq.com>
2019-12-03 02:15:46 +0800

    使用application.yml, 配合@ConfigurationProperties(prefix = "person")注解, 给对象来赋值

---
+++ bootstart/src/main/java/com/zgy/learn/bootstart/pojo/Person.java
@@ -0,0 +1,30 @@
+package com.zgy.learn.bootstart.pojo;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: renjiaxin
+ * @Description:
+ * @Date: 2019-12-03 02:02
+ * @Modified by:
+ */
+@Component
+@ConfigurationProperties(prefix = "person")
+@Data
+public class Person {
+    private  String lastName;
+    private Integer age;
+    private Boolean boos;
+    private Date birthday;
+
+    private Map<String, Object> maps;
+    private List<Object> lists;
+
+    private Dog dog;
+}
