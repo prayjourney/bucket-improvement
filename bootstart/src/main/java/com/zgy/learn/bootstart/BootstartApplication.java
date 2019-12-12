cf72357cee6ee0264c05381bae419e6d0819c6ba
renjiaxin <2247359268@qq.com>
2019-12-12 23:53:36 +0800

    加入日誌,日誌面向slf4j

--- bootstart/src/main/java/com/zgy/learn/bootstart/BootstartApplication.java
+++ bootstart/src/main/java/com/zgy/learn/bootstart/BootstartApplication.java
@@ -1,14 +1,19 @@
 package com.zgy.learn.bootstart;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class BootstartApplication {
+    // 日志记录器, 日志面向slf4j
+    private static Logger logger = LoggerFactory.getLogger(BootstartApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(BootstartApplication.class, args);
+        logger.info("开始启动了....");
     }
 
 }
--- bootstart/src/main/java/com/zgy/learn/bootstart/controller/MyController.java
+++ bootstart/src/main/java/com/zgy/learn/bootstart/controller/MyController.java
@@ -1,8 +1,14 @@
 package com.zgy.learn.bootstart.controller;
 
+import com.zgy.learn.bootstart.BootstartApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author: renjiaxin
@@ -14,8 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 //@RestBody        // json
 @RestController    // controller + json
 public class MyController {
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping("mycon")
-    public String mycontroller(){
-        return "my controller 123!";
+    public String mycontroller() {
+        logger.debug("开始访问mycontroller方法, 直接返回一个json子字符串, 返回的字符串就是retrn内容");
+        // 返回的是json, 而不是一个页面, 所以在templates里面不需要对应名称的html页面
+        Map<String, String> mp = new HashMap<>();
+        mp.put("name", "张三");
+        mp.put("age", "23");
+        mp.put("gender", "男");
+        return "my controller 123!" + mp.values();
     }
 }
