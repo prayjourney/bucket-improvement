2fad523999794d0fc9a59a7b6e59b46ad347326a
renjiaxin <renjiaxin@126.com>
2019-12-13 10:59:47 +0800

    加入了日志配置，但是占位符还没有处理好

--- bootcrud/src/main/java/com/zgy/learn/bootcrud/controller/HelloController.java
+++ bootcrud/src/main/java/com/zgy/learn/bootcrud/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.zgy.learn.bootcrud.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping("hello")
     public String helloController() {
+        logger.debug("访问方法： {0}", "helloController");
         return "hello boot crud!";
     }
 }
