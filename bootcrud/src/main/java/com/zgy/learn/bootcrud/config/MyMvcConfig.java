66d6b0948299fa2fa030440e3b4f3c2d3cdddeb9
zuiguangyin <2247359268@qq.com>
2019-12-14 13:41:49 +0800

    @Override     public void addViewControllers(ViewControllerRegistry registry) {         // super.addViewControllers(registry);         // 浏览器发送 /hello 请求来到 success, 这样就是把请求路径和视图直接绑定了         registry.addViewController("/hello").setViewName("success");     }

--- bootcrud/src/main/java/com/zgy/learn/bootcrud/config/MyMvcConfig.java
+++ bootcrud/src/main/java/com/zgy/learn/bootcrud/config/MyMvcConfig.java
@@ -14,17 +14,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class MyMvcConfig extends WebMvcConfigurerAdapter {
 
 
-
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-       // super.addViewControllers(registry);
-        //浏览器发送 /atguigu 请求来到 success
-        registry.addViewController("/atguigu").setViewName("success");
+        // super.addViewControllers(registry);
+        // 浏览器发送 /hello 请求来到 success, 这样就是把请求路径和视图直接绑定了
+        registry.addViewController("/hello").setViewName("success");
     }
 
     //所有的WebMvcConfigurerAdapter组件都会一起起作用
     @Bean //将组件注册在容器
-    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter() {
         WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
             @Override
             public void addViewControllers(ViewControllerRegistry registry) {
@@ -47,7 +46,7 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
     }
 
     @Bean
-    public LocaleResolver localeResolver(){
+    public LocaleResolver localeResolver() {
 
         return new MyLocaleResolver();
     }
