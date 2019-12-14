ece9de42f27631bce57e711bda63e9612d22bd10
zuiguangyin <2247359268@qq.com>
2019-12-14 17:35:45 +0800

    去除bootstrap的url

--- bootcrud/src/main/java/com/zgy/learn/bootcrud/config/MyMvcConfig.java
+++ bootcrud/src/main/java/com/zgy/learn/bootcrud/config/MyMvcConfig.java
@@ -40,6 +40,7 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
                 //super.addInterceptors(registry);
                 //静态资源；  *.css , *.js
                 //SpringBoot已经做好了静态资源映射
+                // 拦截了所有的请求，除了下面三个路径
                 registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
                         .excludePathPatterns("/login.html","/","/user/login");
             }
