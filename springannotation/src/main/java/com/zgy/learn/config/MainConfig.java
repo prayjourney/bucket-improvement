75b086af89db6586461cf6f1d946ea778f6f3322
zuiguangyin <2247359268@qq.com>
2019-12-27 17:36:48 +0800

    使用@ComponentScan注解，排除了Service和Controller注解标注的bean

--- springannotation/src/main/java/com/zgy/learn/config/MainConfig.java
+++ springannotation/src/main/java/com/zgy/learn/config/MainConfig.java
@@ -4,6 +4,9 @@ import com.zgy.learn.bean.Person;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 /**
  * @Author: renjiaxin
@@ -12,7 +15,17 @@ import org.springframework.context.annotation.Configuration;
  * @Modified by:
  */
 @Configuration  // 表示这是一个注解配置的类，作用等于配置的xml文件
-@ComponentScan(value = "com.zgy.learn")  // 包扫描，指定位置, 指定com.zgy.learn
+@ComponentScan(value = "com.zgy.learn", excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class, Service.class})
+})  // 包扫描，指定位置, 指定com.zgy.learn
+//@ComponentScan  value:指定要扫描的包
+//excludeFilters = Filter[] ：指定扫描的时候按照什么规则排除那些组件
+//includeFilters = Filter[] ：指定扫描的时候只需要包含哪些组件
+//FilterType.ANNOTATION：按照注解
+//FilterType.ASSIGNABLE_TYPE：按照给定的类型；
+//FilterType.ASPECTJ：使用ASPECTJ表达式
+//FilterType.REGEX：使用正则指定
+//FilterType.CUSTOM：使用自定义规则
 public class MainConfig {
 
     // 给容器返回一个bean, 类型是Person，默认的名字是方法名，首字母小写
