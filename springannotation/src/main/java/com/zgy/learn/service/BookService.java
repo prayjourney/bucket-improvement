3ad4dbb8aae50194b529646841bd492cfaf5a2cc
zuiguangyin <2247359268@qq.com>
2019-12-30 15:57:28 +0800

    @Qualifier指定明确的bean名字，感觉不好使啊

--- springannotation/src/main/java/com/zgy/learn/service/BookService.java
+++ springannotation/src/main/java/com/zgy/learn/service/BookService.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 public class BookService {
 
     @Autowired()
-    //@Qualifier("bookDao2")
+    // @Qualifier("bookDao2")
     private BookDao bookDao;
 
     public void print(){
