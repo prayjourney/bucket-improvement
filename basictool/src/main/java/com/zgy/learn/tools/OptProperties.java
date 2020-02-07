f34104c7763de295a9557091c507f5e17c02e12b
zuiguangyin <2247359268@qq.com>
2020-02-07 22:47:11 +0800

    操作properties文件

---
+++ basictool/src/main/java/com/zgy/learn/tools/OptProperties.java
@@ -0,0 +1,93 @@
+package com.zgy.learn.tools;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription: 操作properties文件
+ * @Date: Created in 2020/2/7 15:28
+ * @Modified by:
+ */
+public class OptProperties {
+    public static void getPathTest() {
+        // 取到绝对路径, 但是这个是target里面的, 带了file:
+        URL is = Thread.currentThread().getContextClassLoader().getResource("myinfo.properties");
+        System.out.println(is);
+        // 取到绝对路径, 但是这个是target里面的，去掉了file
+        String path1 = OptProperties.class.getResource("/myinfo.properties").getPath();
+        System.out.println(path1);
+
+        // 获取当前项目的路径
+        String property = System.getProperty("user.dir");
+        System.out.println(property);
+        String mypath = property + "\\basictool\\src\\main\\resources\\myinfo.properties";
+        System.out.println(mypath);
+
+        // 在target目录下的文件名
+        String fileName = OptProperties.class.getClassLoader().getResource("\\myinfo.properties").getPath();
+        System.out.println(fileName);
+    }
+
+    public static void main(String[] args) {
+        //getPathTest();
+        String property =System.getProperty("user.dir");
+        String mypath = property + "\\basictool\\src\\main\\resources\\myinfo.properties";
+        String mypathtemp = property + "\\basictool\\src\\main\\resources\\myinfotemp.properties";
+        System.out.println(mypath);
+        //readProperties(mypath);
+        writeProperties(mypath,mypathtemp);
+
+    }
+
+    public static void readProperties(String name) {
+        try {
+            FileInputStream fis = new FileInputStream(name);
+            Properties pro = new Properties();
+            pro.load(fis);
+            System.out.println(pro.getProperty("age"));
+            System.out.println(pro.getProperty("name"));
+            System.out.println(pro.getProperty("hello"));
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeProperties(String path,String path2) {
+        try {
+            // 读取
+            FileInputStream fis = new FileInputStream(path);
+            // 输出
+            FileOutputStream fos = new FileOutputStream(path2);
+            Properties pro = new Properties();
+            Properties protemp = new Properties();
+            pro.load(fis);
+            fis.close();
+
+            // 先放里面，然后修改
+            protemp.putAll(pro);
+            protemp.setProperty("hello", "123");
+            protemp.store(fos, "write");
+            fos.close();
+
+            // 文件删除和改名
+            File  f1 = new File(path);
+            File  f2 = new File(path2);
+            f1.delete();
+            f2.renameTo(f1);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
