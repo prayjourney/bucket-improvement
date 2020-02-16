bdef804fcf2ee29e83cab3a16b942b32cf993204
zuiguangyin <2247359268@qq.com>
2020-02-16 20:25:18 +0800

    可以正常启动和访问

---
+++ spring-shiro/src/main/java/com/zgy/shiro/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.zgy.shiro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription:
+ * @Date: Created in 2020/2/16 20:05
+ * @Modified by:
+ */
+@Controller
+public class UserController {
+    @RequestMapping(value="/userinfo")
+    public String getUserInfo(HttpServletRequest request){
+        String currentUser = (String)request.getSession().getAttribute("currentUser");
+        System.out.println("当前登录的用户为[" + currentUser + "]");
+        request.setAttribute("currUser", currentUser);
+        return "/user";
+    }
+}
--- spring-shiro/src/main/webapp/login.jsp
+++ spring-shiro/src/main/webapp/login.jsp
@@ -1,5 +1,10 @@
 <html>
 <body>
 <h2>Login page!</h2>
+    <form name="login" method="post" action="/spring_shiro_war_exploded/login" >
+        <input type="text" name="username">
+        <input type="text" name="password">
+        <input type="submit" value="login"/>
+    </form>
 </body>
 </html>
