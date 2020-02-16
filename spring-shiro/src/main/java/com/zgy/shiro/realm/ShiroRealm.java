9b5591d7a52d99edb872a4e0ebea0f1db6a969bf
zuiguangyin <2247359268@qq.com>
2020-02-16 19:42:15 +0800

    No Spring WebApplicationInitializer types detected on classpath 加载不到配置文件或找不到spring主配置

---
+++ spring-shiro/src/main/java/com/zgy/shiro/realm/ShiroRealm.java
@@ -0,0 +1,29 @@
+package com.zgy.shiro.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription:
+ * @Date: Created in 2020/2/16 17:35
+ * @Modified by:
+ */
+public class ShiroRealm implements Realm {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return false;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
