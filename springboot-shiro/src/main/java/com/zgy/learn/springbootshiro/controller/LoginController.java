cc583a83c5f08b1319231e9022ccc2353b28e54b
zuiguangyin <2247359268@qq.com>
2020-02-17 17:12:25 +0800

    可以访问首页了，添加了springmvc的资源配置和信息

---
+++ springboot-shiro/src/main/java/com/zgy/learn/springbootshiro/controller/LoginController.java
@@ -0,0 +1,44 @@
+package com.zgy.learn.springbootshiro.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription: 登录
+ * @Date: Created in 2020/2/17 13:39
+ * @Modified by:
+ */
+@Slf4j
+@Controller
+public class LoginController {
+
+    // Logger log =LoggerFactory.getLogger(LoginController.class);
+    @RequestMapping(value = "login")
+    // public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+    public String login(String username, String password) {
+
+        Subject currentUser = SecurityUtils.getSubject();
+        if (!currentUser.isAuthenticated()) {
+            // 把用户名和密码封装为 UsernamePasswordToken对象
+            UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+            // remember me
+            token.setRememberMe(true);
+            try {
+                // 尝试登录
+                currentUser.login(token);
+            } catch (AuthenticationException e) {
+                System.out.println("登录失败: " + e.getMessage());
+                log.error("登录失败: ", e);
+            }
+        }
+
+        return "list";
+    }
+
+}
