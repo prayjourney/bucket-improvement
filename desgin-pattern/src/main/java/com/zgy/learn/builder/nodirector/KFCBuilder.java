d1e7e2bb497ff93f7677ba143f5439c74cc13df9
zuiguangyin <2247359268@qq.com>
2020-02-24 01:49:25 +0800

    顾客来点餐，就生成了自己的食谱，具体的KFC点餐建造者，消除了构造的顺序

---
+++ desgin-pattern/src/main/java/com/zgy/learn/builder/nodirector/KFCBuilder.java
@@ -0,0 +1,16 @@
+package com.zgy.learn.builder.nodirector;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription: 抽象的KFC点餐建造者
+ * @Date: Created in 2020/2/24 1:33
+ * @Modified by:
+ */
+public abstract class KFCBuilder {
+    abstract  KFCBuilder buildA(String str); // 薯条
+    abstract  KFCBuilder buildB(String str); // 可乐
+    abstract  KFCBuilder buildC(String str); // 炸鸡
+
+    // 获得产品
+    abstract KFCRecipes getRecipes();
+}
---
+++ desgin-pattern/src/main/java/com/zgy/learn/builder/nodirector/KFCRecipes.java
@@ -0,0 +1,35 @@
+package com.zgy.learn.builder.nodirector;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription: KFC菜谱，这个具体的产品
+ * @Date: Created in 2020/2/24 1:34
+ * @Modified by:
+ */
+public class KFCRecipes {
+    // 默认菜谱
+    private String recipe1= "可乐";
+    private String recipe2 = "汉堡";
+    private String recipe3 = "鸡块";
+
+    public void setRecipe1(String recipe1) {
+        this.recipe1 = recipe1;
+    }
+
+    public void setRecipe2(String recipe2) {
+        this.recipe2 = recipe2;
+    }
+
+    public void setRecipe3(String recipe3) {
+        this.recipe3 = recipe3;
+    }
+
+    @Override
+    public String toString() {
+        return "KFCRecipes{" +
+                "recipe1='" + recipe1 + '\'' +
+                ", recipe2='" + recipe2 + '\'' +
+                ", recipe3='" + recipe3 + '\'' +
+                '}';
+    }
+}
