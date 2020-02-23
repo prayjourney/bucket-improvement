06235a46c2f0b6718606d3bbdf39b8d2e013e6f0
zuiguangyin <2247359268@qq.com>
2020-02-24 01:55:42 +0800

    上述是默认的操作， 因为返回的都是Builder， 而且无顺序，还可以链式操作

--- desgin-pattern/src/main/java/com/zgy/learn/builder/nodirector/ClientTest.java
+++ desgin-pattern/src/main/java/com/zgy/learn/builder/nodirector/ClientTest.java
@@ -11,5 +11,10 @@ public class ClientTest {
         CustomerRecipesBuilder builder = new CustomerRecipesBuilder();
         KFCRecipes recipes = builder.getRecipes();
         System.out.println(recipes);
+        // 上述是默认的操作， 因为返回的都是Builder， 而且无顺序，还可以链式操作
+        CustomerRecipesBuilder builder2 = new CustomerRecipesBuilder();
+        builder2.buildA("雪碧").buildB("老八秘制小汉堡").buildC("上校鸡块"); // 链式编程
+        KFCRecipes recipes2 = builder2.getRecipes();
+        System.out.println(recipes2);
     }
 }
