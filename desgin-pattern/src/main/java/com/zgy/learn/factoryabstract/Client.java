dbd1856aef2861c3b6838e2c88900b79e827a201
zuiguangyin <2247359268@qq.com>
2020-02-23 23:18:20 +0800

    抽象工厂的运作和个各类之间的关系图

---
+++ desgin-pattern/src/main/java/com/zgy/learn/factoryabstract/Client.java
@@ -0,0 +1,27 @@
+package com.zgy.learn.factoryabstract;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription:
+ * @Date: Created in 2020/2/23 22:55
+ * @Modified by:
+ */
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("============小米产品===========");
+        AbstractProductFactory xiaomiFactory = new XiaomiFactory();
+        PcProduct pc = xiaomiFactory.createPc();
+        pc.open();
+        pc.playGame();
+        PhoneProduct phone = xiaomiFactory.createPhone();
+        phone.sendSMS();
+        phone.callUp();
+
+        System.out.println("============Apple产品===========");
+        AbstractProductFactory appleFactory = new AppleFactory();
+        PhoneProduct iPhone = appleFactory.createPhone();
+        iPhone.open();
+        iPhone.sendSMS();
+
+    }
+}
