d9dbd219157edfd48a656dcecbbbe91020f770d2
zuiguangyin <2247359268@qq.com>
2020-02-25 03:43:36 +0800

    有多个参数的枚举类

--- basictool/src/main/java/com/zgy/learn/commontest/EnumTest.java
+++ basictool/src/main/java/com/zgy/learn/commontest/EnumTest.java
@@ -28,11 +28,56 @@ enum MyColor {
     }
 }
 
+// 有参数的枚举
+enum Dog {
+    ZH("中华田园犬",998), JP("日本柴犬",720), EN("英国哈士奇"),DE("德国牧羊犬",1433),
+    JP2("日本秋田犬");
+    // 属性: 品种
+    String kind;
+    // 属性: 价格
+    Integer val;
 
+    Dog(String kind) {
+        this.kind = kind;
+    }
+
+    Dog(String kind, Integer val) {
+        this.kind = kind;
+        this.val = val;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public Integer getVal() {
+        return val;
+    }
+
+    public void setVal(Integer val) {
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "kind='" + kind + '\'' +
+                ", val=" + val +
+                '}';
+    }
+}
 public class EnumTest {
     public static void main(String[] args) {
         System.out.println("我最喜欢的颜色：" + MyColor.RED.name);
         System.out.println("test: " + MyEnum01.BLUE);
+
+        // Dog.valueOf("中华田园犬"); // 错误
+        System.out.println(Dog.valueOf("ZH"));
+        System.out.println(Dog.JP.getKind()+ "的价格是: "  + Dog.JP.getVal());
     }
 
 }
