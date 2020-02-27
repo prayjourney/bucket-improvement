ca0103d352c92693ff098731c07c9e25168a51ce
zuiguangyin <2247359268@qq.com>
2020-02-27 23:12:29 +0800

    使用组合的方式，来完成了适配器模式，面向接口编程的思想符合

--- desgin-pattern/src/main/java/com/zgy/learn/structuretype/adapter/Usb2WireNetAdapter.java
+++ desgin-pattern/src/main/java/com/zgy/learn/structuretype/adapter/Usb2WireNetAdapter.java
@@ -6,10 +6,18 @@ package com.zgy.learn.structuretype.adapter;
  * @Date: Created in 2020/2/27 22:49
  * @Modified by:
  */
-public class Usb2WireNetAdapter extends WireAdaptee implements NetAdapter {
-    // 使用继承的方式来实现上网，这样不好
+public class Usb2WireNetAdapter implements NetAdapter {
+    // 使用组合的方式，来完成了适配器模式，面向接口编程的思想符合
+    WireAdaptee wire = new WireAdaptee();
     @Override
     public void handler() {
-        super.goToNet();
+        wire.goToNet();
     }
+//public class Usb2WireNetAdapter extends WireAdaptee implements NetAdapter {
+//    // 使用继承的方式来实现上网，这样不好
+//    @Override
+//    public void handler() {
+//        super.goToNet();
+//    }
+
 }
