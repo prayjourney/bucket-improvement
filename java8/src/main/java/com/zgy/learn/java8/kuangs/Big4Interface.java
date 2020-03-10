9d4101639101b371b8d526cd03e73609ad20be0b
zuiguangyin <2247359268@qq.com>
2020-03-10 22:25:44 +0800

    四种基本的函数式接口

--- java8/src/main/java/com/zgy/learn/java8/kuangs/Big4Interface.java
+++ java8/src/main/java/com/zgy/learn/java8/kuangs/Big4Interface.java
@@ -1,7 +1,10 @@
 package com.zgy.learn.java8.kuangs;
 
+import java.util.UUID;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 /**
  * @Author: renjiaxin
@@ -17,11 +20,13 @@ public class Big4Interface {
     public static void main(String[] args) {
         usePredicate();
         useFunction();
+        useSupplier();
+        useConsume();
 
     }
 
     /**
-     * 段言行接口，返回一个布尔值，一个参数，Predicate
+     * 断言型接口，返回一个布尔值，一个参数，Predicate
      */
     public static void usePredicate() {
         // 传统方式
@@ -57,6 +62,41 @@ public class Big4Interface {
             return s1.length();
         };
         System.out.println(function2.apply("renjiaxin!!!"));
+    }
 
+    // 供给型，无参数，有一个返回值
+    public static void useSupplier() {
+        // 传统
+        Supplier<String> supplier = new Supplier<String>() {
+            @Override
+            public String get() {
+                return UUID.randomUUID().toString();
+            }
+        };
+        System.out.println(supplier.get());
+
+        // lambda
+        Supplier<String> supplier1 = () -> {
+            return UUID.randomUUID().toString();
+        };
+        System.out.println(supplier1.get());
+    }
+
+    // 消费型，一个参数，无返回值
+    public static void useConsume() {
+        // 传统
+        Consumer<String> consumer = new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s.length());
+            }
+        };
+        consumer.accept("zhangsanfeng!");
+
+        // lambda
+        Consumer<String> consumer2 = (str) -> {
+            System.out.println(str.length());
+        };
+        consumer2.accept("123321312312");
     }
 }
