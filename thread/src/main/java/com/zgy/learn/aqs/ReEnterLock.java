e2796b3afd03a7d4a9ef553e169524183b2fc076
zuiguangyin <2247359268@qq.com>
2020-03-11 02:47:35 +0800

    可重入锁，拿到了外面的锁，就自动拿到了里面的锁，可以减少死锁的情况，死锁四个条件：1.互斥，2.不可剥夺，3.占有等待，4.循环等待！

---
+++ thread/src/main/java/com/zgy/learn/aqs/ReEnterLock.java
@@ -0,0 +1,28 @@
+package com.zgy.learn.aqs;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription: 可重入锁，synchronized和Lock都是可重入锁，可重入锁可以一定程度上减缓死锁，以synchronized为例子
+ * @Date: Created in 2020/3/11 2:41
+ * @Modified by:
+ */
+public class ReEnterLock {
+    public static void main(String[] args) throws InterruptedException {
+        new ReEnterLock().testA();
+
+    }
+
+    // 也叫递归锁，其实就是，拿到了外面的锁，里面的锁自动拿到
+    synchronized void testA() throws InterruptedException {
+        System.out.println("hello 我是A");
+        testB();
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("What The FUCK!");
+    }
+
+    synchronized void testB() {
+        System.out.println("hello 我是B");
+    }
+}
