003068c780ad9242f91a696afdf6462513b021af
zuiguangyin <2247359268@qq.com>
2020-03-28 23:35:22 +0800

    这样有问题的

--- thread/src/main/java/com/zgy/learn/aqs/ReadWriteLockDemo.java
+++ thread/src/main/java/com/zgy/learn/aqs/ReadWriteLockDemo.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
@@ -19,9 +21,9 @@ public class ReadWriteLockDemo {
         for (int i = 0; i < 5; i++) {
             final int x = i;
             new Thread(() -> {
-                System.out.println(Thread.currentThread().getName() + "设置");
+                System.out.println(Thread.currentThread().getName() + ": 设置");
                 cache.set(x + "", x + "");
-                System.out.println(Thread.currentThread().getName() + "设置完毕" + x
+                System.out.println(Thread.currentThread().getName() + ": 设置完毕, " + x
                         + LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
             }, "写入线程：" + String.valueOf(x)).start();
         }
@@ -30,9 +32,9 @@ public class ReadWriteLockDemo {
         for (int i = 0; i < 5; i++) {
             final int x = i;
             new Thread(() -> {
-                System.out.println(Thread.currentThread().getName() + "读取");
+                System.out.println(Thread.currentThread().getName() + ": 读取");
                 cache.get(x + "");
-                System.out.println(Thread.currentThread().getName() + "读取完毕" + x
+                System.out.println(Thread.currentThread().getName() + ": 读取完毕, " + x
                         + LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
             }, "读取线程：" + String.valueOf(x)).start();
         }
@@ -43,14 +45,22 @@ public class ReadWriteLockDemo {
  * 资源类，一个缓存
  */
 class MyCache {
-    public volatile Map<String, Object> mycache = new HashMap<>();
-    ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    public  Map<String, Object> mycache = new HashMap<>();
+    ReadWriteLock lock = new ReentrantReadWriteLock();
 
     /**
-     * 读取缓存，现在这样，写锁上了锁，而读锁没有上锁，这样会导致读和写不互斥，所以会出现问题
+     * 读取缓存，读读共享，读写互斥
+     * //现在这样，写锁上了锁，而读锁没有上锁，这样会导致读和写不互斥，所以会出现问题
      */
     public Object get(String key) {
-        return mycache.get(key);
+//        // 上锁
+//        lock.readLock().lock();
+//        try{
+            return mycache.get(key);
+//        }finally {
+//            // 开锁
+//            lock.readLock().unlock();
+//        }
     }
 
     /**
@@ -58,14 +68,16 @@ class MyCache {
      */
     public void set(String key, Object value) {
         // 上锁
-        lock.writeLock();
+        lock.writeLock().lock();
         try{
+            TimeUnit.MILLISECONDS.sleep(200);
             mycache.put(key, value);
-        }finally {
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
             // 开锁
             lock.writeLock().unlock();
         }
-
     }
 
 }
