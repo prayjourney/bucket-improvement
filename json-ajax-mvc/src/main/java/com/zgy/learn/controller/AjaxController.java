a076e314c304c91a325274549bca443eb4424058
zuiguangyin <2247359268@qq.com>
2020-04-13 00:34:42 +0800

    update

--- json-ajax-mvc/src/main/java/com/zgy/learn/controller/AjaxController.java
+++ json-ajax-mvc/src/main/java/com/zgy/learn/controller/AjaxController.java
@@ -1,11 +1,8 @@
 package com.zgy.learn.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zgy.learn.pojo.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
