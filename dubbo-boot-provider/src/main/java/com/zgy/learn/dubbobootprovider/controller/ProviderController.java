9ee3fb0829168145b60b0f341c7b02521dcd9737
zuiguangyin <2247359268@qq.com>
2020-07-05 22:50:12 +0800

    增加了controller

---
+++ dubbo-boot-provider/src/main/java/com/zgy/learn/dubbobootprovider/controller/ProviderController.java
@@ -0,0 +1,19 @@
+package com.zgy.learn.dubbobootprovider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription:
+ * @Date: Created in 2020/7/5 22:39
+ * @Modified by:
+ */
+@RestController
+public class ProviderController {
+
+    @GetMapping(value = {"/","index","test"})
+    public String testProvider(){
+        return "我是服务提供者！";
+    }
+}
