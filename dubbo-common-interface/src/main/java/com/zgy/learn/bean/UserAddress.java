cae0eb54aa170b621199695dd7757bcda99e42ce
zuiguangyin <2247359268@qq.com>
2020-07-05 13:00:27 +0800

    bean和service，使用lombok简化

--- dubbo-common-interface/src/main/java/com/zgy/learn/bean/UserAddress.java
+++ dubbo-common-interface/src/main/java/com/zgy/learn/bean/UserAddress.java
@@ -6,6 +6,9 @@ package com.zgy.learn.bean;
  * @Date: Created in 2020/1/2 1:03
  * @Modified by:
  */
+import lombok.Builder;
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
@@ -13,18 +16,23 @@ import java.io.Serializable;
  * @author lfy
  *
  */
+@Data
+@Builder
 public class UserAddress implements Serializable {
-
     private Integer id;
-    private String userAddress; //用户地址
-    private String userId;      //用户id
-    private String consignee;   //收货人
-    private String phoneNum;    //电话号码
-    private String isDefault;   //是否为默认地址    Y-是     N-否
+    // 用户地址
+    private String userAddress;
+    // 用户id
+    private String userId;
+    // 收货人
+    private String consignee;
+    // 电话号码
+    private String phoneNum;
+    // 是否为默认地址    Y-是     N-否
+    private String isDefault;
 
     public UserAddress() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     public UserAddress(Integer id, String userAddress, String userId, String consignee, String phoneNum,
@@ -37,45 +45,4 @@ public class UserAddress implements Serializable {
         this.phoneNum = phoneNum;
         this.isDefault = isDefault;
     }
-
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public String getUserAddress() {
-        return userAddress;
-    }
-    public void setUserAddress(String userAddress) {
-        this.userAddress = userAddress;
-    }
-    public String getUserId() {
-        return userId;
-    }
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-    public String getConsignee() {
-        return consignee;
-    }
-    public void setConsignee(String consignee) {
-        this.consignee = consignee;
-    }
-    public String getPhoneNum() {
-        return phoneNum;
-    }
-    public void setPhoneNum(String phoneNum) {
-        this.phoneNum = phoneNum;
-    }
-    public String getIsDefault() {
-        return isDefault;
-    }
-    public void setIsDefault(String isDefault) {
-        this.isDefault = isDefault;
-    }
-
-
-
-
 }
