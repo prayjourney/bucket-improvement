56826ba6ce630a056c99bb50d57cba7ce2245da0
pray-journey.io <2247359268@qq.com>
2021-04-26 00:57:26 +0800

    🎉🎉🎉update

--- dubbo-boot-provider/src/test/java/com/zgy/learn/dubbobootprovider/DubboBootProviderApplicationTests.java
+++ dubbo-boot-provider/src/test/java/com/zgy/learn/dubbobootprovider/DubboBootProviderApplicationTests.java
@@ -1,7 +1,6 @@
 package com.zgy.learn.dubbobootprovider;
 
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
